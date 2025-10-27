010e29cfb04f2363ae3daf19b93e15d4e0ec431b
Md. Samiul Islam Sabbir <166505679+Sabbirraj404@users.noreply.github.com>
2025-10-27 17:02:11 +0600

    Add files via upload

---
+++ Bluetooth_control_car_with_2WD_kit.ino
@@ -0,0 +1,69 @@
+/*Bluetooth control car with Arduino Nano board
+   https://srituhobby.com
+*/
+
+//motor one
+#define IN1 2
+#define IN2 3
+
+//motor two
+#define IN3 4
+#define IN4 5
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    char value = Serial.read();
+    Serial.println(value);
+
+    if (value == 'F') {
+      Forward();
+    } else if (value == 'B') {
+      Backward();
+    } else if (value == 'S') {
+      Stop();
+    } else if (value == 'L') {
+      Left();
+    } else if (value == 'R') {
+      Right();
+    }
+  }
+}
+
+void Forward() {
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, HIGH);
+}
+void Backward() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+}
+void Stop() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+}
+void Left() {
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+}
+void Right() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+}
